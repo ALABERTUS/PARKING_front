b3413ae65926af9d9c1efa8e1e3db9774a8c619d
yueming <bluemoon_cn@hotmail.com>
2023-10-20 08:40:29 +0200

    review

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Index  from './layout/Index.jsx'
-import Auth0ProviderWithNavigare from './layout/Auth0ProviderWithNavigate.jsx';
+import Auth0ProviderWithNavigate from './layout/Auth0ProviderWithNavigate.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0ProviderWithNavigare>
+    <Auth0ProviderWithNavigate>
     <Index />
-    </Auth0ProviderWithNavigare>
+    </Auth0ProviderWithNavigate>
   </React.StrictMode>
 );
